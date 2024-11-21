95981f3efc8e1b1e7117a66fc2c2567301950cfd
without eyes <37347347+without-eyes@users.noreply.github.com>
2024-11-21 13:19:12 +0200

    Add config file for database connection

---
+++ src/backend/config.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_USER = os.getenv("DB_USER", "root")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "password")
+DB_NAME = os.getenv("DB_NAME", "DarkAndDarkerStats")
