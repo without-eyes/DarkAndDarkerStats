6dfffbd8199a88caffbbc87dba6ff63968eb6101
without eyes <37347347+without-eyes@users.noreply.github.com>
2024-12-12 22:02:38 +0200

    Add working match info

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -45,22 +45,3 @@
 function navigateTo(page) {
     window.location.href = page;
 }
-
-async function getMatchDetails() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const matchId = urlParams.get('id');
-
-    try {
-        const response = await fetch(`http://localhost:5000/api/match/${matchId}`);
-        if (!response.ok) throw new Error('Failed to fetch match details');
-        const match = await response.json();
-        document.getElementById('matchId').textContent = `Match #${match.match_id}`;
-        document.getElementById('matchDate').textContent = `Date: ${match.start_time} to ${match.end_time}`;
-        document.getElementById('matchKills').textContent = `Kills: ${match.kills}`;
-        document.getElementById('matchEscaped').textContent = `Escaped: ${match.escaped}`;
-    } catch (error) {
-        console.error('Error fetching match details:', error);
-        document.getElementById('matchId').textContent = 'Error fetching match details';
-    }
-}
-
