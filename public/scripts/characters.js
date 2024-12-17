f5e0d81c059e0fdc23e1d44a06c039acd208db2d
without eyes <37347347+without-eyes@users.noreply.github.com>
2024-12-17 13:45:52 +0200

    Add page for all user's characters

---
+++ public/scripts/characters.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", async () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const userId = urlParams.get('id');
+
+    try {
+        const charactersResponse = await fetch(`http://localhost:5000/api/user/${userId}/characters`);
+        if (!charactersResponse.ok) throw new Error(`Failed to fetch characters: ${charactersResponse.status}`);
+        const characters = await charactersResponse.json();
+
+        const charactersList = document.getElementById('charactersList');
+        charactersList.innerHTML = '';
+        characters.slice(0, 5).forEach(character => {
+            const li = document.createElement('li');
+            li.innerHTML = `<a href="character.html?userId=${userId}&id=${character.id}">${character.name}</a>`;
+            charactersList.appendChild(li);
+        });
+    } catch (error) {
+        console.error('Error fetching characters:', error);
+    }
+});
