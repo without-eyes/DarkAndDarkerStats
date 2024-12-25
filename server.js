424b8fb19f08a061218b5a892b98365ec20b559b
without eyes <37347347+without-eyes@users.noreply.github.com>
2024-12-25 14:15:42 +0200

    Attempt to make dynamic IP

--- server.js
+++ server.js
@@ -9,20 +9,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.post('/api/register', (req, res) => {
-    const { username, email, password, confirm_password } = req.body;
-
-    if (!username || !email || !password || !confirm_password) {
-        return res.status(400).json({ message: 'All fields are required' });
-    }
-
-    if (password !== confirm_password) {
-        return res.status(400).json({ message: 'Passwords do not match' });
-    }
-
-    res.status(201).json({ message: 'User registered successfully' });
-});
-
-app.listen(PORT, () => {
-    console.log(`Сервер запущено на http://localhost:${PORT}/index.html`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Сервер запущено на http://localhost:${PORT}/`);
 });
